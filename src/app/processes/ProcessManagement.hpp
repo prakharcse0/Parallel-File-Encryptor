b7a57ccb4859824b293ec287818601998c9e7cee
prakharcse0 <prakharcse0@gmail.com>
2025-05-18 14:16:17 +0530

    Implement multiprocessing

--- src/app/processes/ProcessManagement.hpp
+++ src/app/processes/ProcessManagement.hpp
@@ -3,16 +3,33 @@
 #include "Task.hpp"
 #include <queue>
 #include <memory>
+#include <atomic>
+#include <semaphore.h>
+#include <mutex>
 
 class ProcessManagement {
+    sem_t* itemsSemaphore;
+    sem_t* emptySlotsSemaphore;
 
-    public:
-        ProcessManagement();
-        bool submitToQueue(std::unique_ptr<Task> task);
-        void executeTasks();
-    
-    private:
-        std::queue<std::unique_ptr<Task>> taskQueue;
+public:
+    ProcessManagement();
+    ~ProcessManagement();
+    bool submitToQueue(std::unique_ptr<Task> task);
+    void executeTasks();
+
+private:
+    // std::queue<std::unique_ptr<Task>> taskQueue;
+    struct SharedMemory {
+        std::atomic<size_t> size;
+        char tasks[1000][256];
+        int front;
+        int rear;
+    };
+
+    SharedMemory *sharedMem;
+    int shmFd;
+    const char *SHM_NAME = "/my_queue";
+    std::mutex queueLock;
 };
 
 #endif
